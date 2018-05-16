a661593b22b901dd343d05016a4e275bcb196a26
Werberth Vinícius <werberthvinicius@gmail.com>
2018-05-16 19:30:00 -0300

    Add Movie model

--- movies/movie/models.py
+++ movies/movie/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=150)
+    director = models.CharField(max_length=150)
+    year = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        '{0} [{1}]'.format(self.title, self.year)
